33d8f614ed74bd83ef4503676790fa6e612c8073
Ryoji <b1600579@gl.aiu.ac.jp>
2018-10-29 18:12:27 +0100

    fix Sidebar.vue and test App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <sideBar></sideBar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import sideBar from './components/Sidebar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    sideBar
+  }
 }
 </script>
 
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,16 +1,36 @@
 <template lang="html">
+  <div class="sidebar">
+    <nav>
+      <router-link to="/"><h1>Ryoji<br>Kono</h1></router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/works">Works</router-link>
+      <router-link to="/skills">Skills</router-link>
+    </nav>
+    <ul>
+      <li>Twitter</li>
+      <li>Facebook</li>
+      <li>Github</li>
+    </ul>
+  </div>
 
 </template>
 
 <script>
 export default {
-  name:'Sidebar',
-  data(){
+  name: 'sideBar',
+  data () {
+    return {
 
+    }
   }
 }
 </script>
 
-<style scoped lang="scss">
-
+<style scoped>
+.sidebar{
+  position: absolute;
+  top: 50px;
+  left: 50px;
+  font-size: 1.5rem;
+}
 </style>
