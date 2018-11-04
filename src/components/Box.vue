ec0c4e65a9ab86e38f6f50b04e36c393963672c2
Ryoji <b1600579@gl.aiu.ac.jp>
2018-11-04 18:36:50 +0100

    add box for works page

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="work-box">
+    <h2>{{contents.name}}</h2>
+    <p>{{contents.content}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'box',
+  props: ['contents']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/mixins.scss';
+
+.work-box{
+  @include box(300px, 300px);
+}
+</style>
